cabb2f222c32594ed803254b86ce54cb8880522a
Andrii <91153388+WeAreTheSameBlood@users.noreply.github.com>
2022-04-04 00:40:53 +0300

    Initial commit

---
+++ src/main/java/org/hlybchenko/TestBean.java
@@ -0,0 +1,17 @@
+package org.hlybchenko;
+
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
