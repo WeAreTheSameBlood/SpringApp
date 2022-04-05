28599ee394c39b87bb57ad4713aa34607a89b756
Andrii <91153388+WeAreTheSameBlood@users.noreply.github.com>
2022-04-05 21:16:03 +0300

    Added Dependency Injection.

--- src/main/java/org/hlybchenko/TestSpring.java
+++ src/main/java/org/hlybchenko/TestSpring.java
@@ -5,8 +5,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        //Music music = context.getBean("musicBean", Music.class);
+        //MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
